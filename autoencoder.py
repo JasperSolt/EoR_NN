875332eacaaa781f02516a269906233f18c14743
Jasper Solt <jasper_solt@brown.edu>
2024-08-23 17:13:12 -0400

    setting up infrastructure for VAE

--- autoencoder.py
+++ autoencoder.py
@@ -5,50 +5,49 @@ from torch.utils.data import DataLoader, Dataset
 
 #####
 #
-# Layers
+# Encoder: 
 #
 #####
-def stacked_conv(inchannels, outchannels):
-    layer = nn.Sequential(
-            nn.Conv2d(inchannels, outchannels, 3, padding='same'),
-            nn.ReLU(),
-            nn.BatchNorm2d(outchannels),
-            nn.MaxPool2d(2),
-    )
-    return layer
+class encoder(nn.Module):
+    def __init__(self, input_dim, hidden_dim_1, hidden_dim_2, latent_dim, kernel_size=3):
+        super().__init__()
+        self.conv1 = nn.Conv2d(input_dim, hidden_dim_1, kernel_size, padding='same')
+        self.conv2 = nn.Conv2d(hidden_dim_1, hidden_dim_2, kernel_size, padding='same')
+        self.conv3 = nn.Conv2d(hidden_dim_2, latent_dim, kernel_size, padding='same')
+        
+        self.relu = nn.ReLU()
+        self.pool = nn.MaxPool2d(2)
+        #self.batch_norm = nn.LazyBatchNorm2d()
+        #self.global_pool = nn.AdaptiveAvgPool2d(1)
+
+    def forward(self, x):
+        x1 = self.pool(self.relu(self.conv1(x)))
+        x2 = self.pool(self.relu(self.conv2(x1)))
+        x3 = self.pool(self.relu(self.conv3(x2)))
+        return x3
+
 
 #####
 #
-# Loss Functions: 
+# Decoder: 
 #
 #####
-def weighted_mse_loss(input, target, weight):
-    return (weight * (input - target) ** 2).mean()
+class decoder(nn.Module):
+    def __init__(self, latent_dim, hidden_dim_1, hidden_dim_2, output_dim, kernel_size=3):
+        super().__init__()
+        self.conv1 = nn.Conv2d(latent_dim, hidden_dim_1, kernel_size, padding='same')
+        self.conv2 = nn.Conv2d(hidden_dim_1, hidden_dim_2, kernel_size, padding='same')
+        self.conv3 = nn.Conv2d(hidden_dim_2, output_dim, kernel_size, padding='same')
+        
+        self.relu = nn.ReLU()
+        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear')
 
 
-def corrcoef_loss(input, target):    
-    # Covariance
-    X = torch.cat((input, target), dim=-2)
-    X -= torch.mean(X, -1, keepdim=True)
-    X_T = torch.transpose(X, -2, -1)
-    c = torch.matmul(X, X_T) / (X.shape[-1] - 1)
-
-    # Correlation Coefficient
-    d = torch.diagonal(c, dim1=-1, dim2=-2)
-    stddev = torch.sqrt(d)
-    stddev = torch.where(stddev == 0, 1, stddev)
-    c /= stddev[:,:,:,None]
-    c /= stddev[:,:,None,:]
-
-    #1 - Cross-Correlation Diagonal
-    ccd = 1-torch.diagonal(c, offset=c.shape[-1]//2, dim1=-1, dim2=-2)
-    
-    return ccd.mean()
-
-def ccmse_loss(input, target, alpha=1.0):
-    mse = ((input - target) ** 2).mean()
-    cc = corrcoef_loss(input, target)
-    return  cc + alpha*mse
+    def forward(self, x):
+        x1 = self.relu(self.conv1(self.upsample(x)))
+        x2 = self.relu(self.conv2(self.upsample(x1)))
+        x3 = self.relu(self.conv3(self.upsample(x2)))
+        return x3
 
 
 #####
@@ -57,29 +56,16 @@ def ccmse_loss(input, target, alpha=1.0):
 #
 #####
 class autoencoder(nn.Module):
-    def __init__(self, c=30):
+    def __init__(self, enc, dec):
         super().__init__()
-        self.encoder = nn.Sequential(
-            stacked_conv(c, 16), #30 x 256 x 256 -> 16 x 128 x 128
-            stacked_conv(16, 32), #16 x 128 x 128 -> 32 x 64 x 64
-            stacked_conv(32, 64), #32 x 64 x 64 -> 64 x 32 x 32 
-        )
-        
-        self.decoder = nn.Sequential(
-            nn.Upsample(scale_factor=2, mode='bilinear'),
-            nn.Conv2d(64, 32, 3, padding='same'),
-            nn.ReLU(),
-            
-            nn.Upsample(scale_factor=2, mode='bilinear'),
-            nn.Conv2d(32, 16, 3, padding='same'),
-            nn.ReLU(),
-            
-            nn.Upsample(scale_factor=2, mode='bilinear'),
-            nn.Conv2d(16, c, 3, padding='same'),
-            nn.Sigmoid()
-        )
-
+        self.encoder = enc
+        self.decoder = dec
         
     def forward(self, x):
         return self.decoder(self.encoder(x))
 
+    def encode(self, x):
+        return self.encoder(x)
+
+    def decode(self, x):
+        return self.decoder(x)
