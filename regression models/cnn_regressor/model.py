76253c050b81a2ed243a385e248e8a48a419e718
JasperSolt <jasper_solt@brown.edu>
2024-12-28 14:52:59 -0500

    reorganizing

--- regression models/cnn_regressor/model.py
+++ regression models/cnn_regressor/model.py
@@ -5,7 +5,8 @@ from torch import nn
 from torch.utils.data import DataLoader, Dataset
 #from accelerate import Accelerator
 from hyperparams import Model_Hyperparameters as hp
-
+from EoR_Dataset import EORImageDataset
+from plotting import plot_loss
 
 #model class
 class Fourier_NN(nn.Module):
@@ -137,3 +138,74 @@ def load(model):
         print("Model loaded.")
     else:
         print("Cannot find model path!")
+
+
+
+
+def train_Fourier_NN(hp, init_weights=None):
+    #make sure we aren't overwriting
+    if os.path.isdir(hp.MODEL_DIR):
+        print(hp.MODEL_DIR + " already exists. Please rename current model or delete old model directory.")
+    else:
+
+
+        lossplt_fname = hp.MODEL_DIR + "/" + hp.MODEL_NAME + "_loss.png"
+
+        # training & testing datasets
+        print("Loading training data...")
+        train_data = EORImageDataset("train", hp.TRAINING_DATA_HP)
+        print("Loading validation data...")
+        val_data = EORImageDataset("val", hp.TRAINING_DATA_HP) 
+
+        # training & testing dataloaders
+        train_dataloader = DataLoader(train_data, batch_size=hp.BATCHSIZE, shuffle=True)
+        val_dataloader = DataLoader(val_data, batch_size=hp.BATCHSIZE, shuffle=True)
+
+        # initialize model, optimizer
+        model = Fourier_NN(hp).to("cuda")
+        optim = hp.optimizer(model)
+        
+        #initialize scheduler
+        scheduler = hp.scheduler(optim)
+        
+        if init_weights:
+            print(f"Loading model state from {init_weights}")
+            model.load_state_dict(torch.load(init_weights))
+        
+        #train / test loop
+        loss = { "train" : [], "val" : [] }
+        for t in range(hp.EPOCHS):
+            print(f"Epoch {t+1}\n-------------------------------")
+            loss["train"].append(train(train_dataloader, model, optim))
+            loss["val"].append(test(val_dataloader, model))
+            if hp.LR_DECAY:
+                scheduler.step()
+                print("Learning Rate: {}".format(optim.param_groups[0]['lr']))
+
+            if t in hp.SAVE_EPOCHS:
+                save(model, f"{hp.MODEL_NAME}_{t}")
+                save(model, hp.MODEL_NAME)
+                save_loss(loss, hp)
+                plot_loss(loss, lossplt_fname, f"{hp.MODEL_NAME} Loss")
+
+        save(model, hp.MODEL_NAME)
+        save_loss(loss, hp)
+        plot_loss(loss, lossplt_fname, f"{hp.MODEL_NAME} Loss")
+
+        hp.save_hyparam_summary()
+
+def predict_Fourier_NN(hp_model, hp_test, mode="test"):
+    data = EORImageDataset(mode, hp_test)
+    loader = DataLoader(data, batch_size=hp_test.BATCHSIZE, shuffle=True)
+
+    net = Fourier_NN(hp_model).cuda()
+    load(net)
+
+    predictions, labels = predict(loader, net)
+   
+    #save prediction
+    f = f'{hp_model.MODEL_DIR}/pred_{hp_model.MODEL_NAME}_on_{hp_test.SIMS[0]}_{mode}.npz'
+    np.savez(f, targets=labels, predictions=predictions)
+    
+    print("Prediction saved.")
+    return f
